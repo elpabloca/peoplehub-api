de84068b3c027ad5c4afead63ae1107f6459669c
Pablo Calle <callejupa@gmail.com>
2026-03-19 18:26:47 -0500

    add configuration for db

---
+++ src/env.model.ts
@@ -0,0 +1,8 @@
+export interface Env {
+  PEOPLE_HUB_REQRES_APY_KEY: string;
+  POSTGRES_HOST: string;
+  POSTGRES_PORT: number;
+  POSTGRES_DB: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+}
